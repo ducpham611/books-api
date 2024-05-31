2522fa44cc8b78c02ecdcf3bd45c49c10cd68397
ducpham611 <ducpham6112000@gmail.com>
2024-05-31 11:14:53 +0700

    Change database address

--- src/main/java/com/example/books_api/entity/Author.java
+++ src/main/java/com/example/books_api/entity/Author.java
@@ -1,10 +1,12 @@
 package com.example.books_api.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
